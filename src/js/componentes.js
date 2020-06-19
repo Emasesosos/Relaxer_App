d66cd1d9a812aecb059990c55faa4715ecdaad0d
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2020-06-18 22:49:06 -0500

    Breath Animation With JS Trigger

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,14 +1,42 @@
 /* CSS */
 import './../css/componentes.css';
 /* DOM */
+const container = document.getElementById('container');
+const text = document.getElementById('text');
 
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+/* Breath Animation */
+const breathAnimation = () => {
+
+    text.innerText = 'Breathe In!';
+    container.className = 'container grow';
+
+    setTimeout(() => {
+
+        text.innerText = 'Hold';
+
+        setTimeout(() => {
+
+            text.innerText = 'Breathe Out!';
+            container.className = 'container shrink';
+
+        }, holdTime);
+
+    }, breatheTime);
+
+};
 /* ************************************************************ */
 const events = () => {
     console.log('Event Listeners');
+    setInterval(breathAnimation, totalTime);
 };
 /* ************************************************************ */
 const init = () => {
     console.log('Relaxer App');
+    breathAnimation();
     events();
 };
 /* ************************************************************ */
